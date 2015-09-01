5edd4e01a7b2f607d2d6ced1b27473b7a1a6039b
John Firebaugh <john.firebaugh@gmail.com>
2015-09-01 15:07:17 +0300

    [core] Render from the main thread
    Do not create a thread for the MapContext anymore.

--- src/mbgl/map/map_context.hpp
+++ src/mbgl/map/map_context.hpp
@@ -35,8 +35,6 @@ public:
 
     MapData& getData() { return data; }
 
-    void pause();
-
     void triggerUpdate(const TransformState&, Update = Update::Nothing);
     void renderStill(const TransformState&, const FrameData&, Map::StillImageCallback callback);
 
@@ -85,7 +83,6 @@ private:
 
     Update updateFlags = Update::Nothing;
     util::AsyncTask asyncUpdate;
-    util::AsyncTask asyncInvalidate;
 
     std::unique_ptr<TexturePool> texturePool;
     std::unique_ptr<Painter> painter;
